56814658e7b86f38dfc860b60884e08abdaaaa69
bulabd <bulabd@outlook.com>
2022-07-03 11:31:55 -0400

    Solved intToRoman

---
+++ intToRoman.js
@@ -0,0 +1,16 @@
+const intToRoman = function(num) {
+  const val = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  const rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+
+  
+  let ans = "";
+  for (let i = 0; num; i++) {
+    while (num >= val[i]) ans += rom[i], num -= val[i];
+  }
+  console.log(ans);
+  return ans;
+};
+
+intToRoman(3);
+intToRoman(58);
+intToRoman(1994);
